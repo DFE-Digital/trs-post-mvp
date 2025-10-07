833cfadae867bb3e62410aa12ee00385b9047e4a
pollydrews <136692214+pollydrews@users.noreply.github.com>
2025-10-07 14:57:24 +0100

    Committing missing work

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,20 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+/// ITRs duplicates merge record route
+router.post('/select-option', (req, res) => {
+  const userChoice = req.body.mergeRecord;
+
+  if (userChoice === 'Merge it with Record A') {
+    res.redirect('/itr-duplicates/select');
+  } 
+  else if (userChoice === 'Keep it as a separate record') {
+    res.redirect('/itr-duplicates/keep-separate-reasons');
+  } 
+  else {
+    // fallback if nothing selected
+    res.redirect('/itr-duplicates/select');
+  }
+});
